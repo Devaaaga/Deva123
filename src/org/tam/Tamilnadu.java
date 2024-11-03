096c3cdd73d698efc9bf5e679d4790f9b984e2cd
Devaaaga <devaagar914@gmail.com>
2024-11-03 20:07:39 +0530

    done

---
+++ src/org/tam/Tamilnadu.java
@@ -0,0 +1,12 @@
+package org.tam;
+
+public class Tamilnadu {
+	public void tamilReligion() {
+		System.out.println("tamilreligion:Beautiful");
+	}
+	public void tamilfestivals() {
+		System.out.println("tamilfestival:Pongal");
+	}
+	
+
+}
